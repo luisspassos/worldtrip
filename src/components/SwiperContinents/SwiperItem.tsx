457959dc5fe142a4a5df59decdf761747d1efaba
luis <luis.passos013@gmail.com>
2022-02-25 22:20:18 -0300

    testing text shadow in slider

--- src/components/SwiperContinents/SwiperItem.tsx
+++ src/components/SwiperContinents/SwiperItem.tsx
@@ -13,9 +13,12 @@ export function SwiperItem({ img, continent, subtitle }: SwiperItemProps) {
       gap="4"
       h="100%"
       bgImage={`url(${img})`}
+      bgSize="cover"
+      bgPosition="center"
     >
       <Heading
         fontSize="3rem"
+        textShadow="dark-lg"
       >
         {continent}
       </Heading>
@@ -23,6 +26,7 @@ export function SwiperItem({ img, continent, subtitle }: SwiperItemProps) {
         as="h3"
         color="gray.400"
         fontSize="1.7rem"
+        textShadow="1px 2px 2px #09"
       >
         {subtitle}
       </Heading>
--- src/components/SwiperContinents/index.tsx
+++ src/components/SwiperContinents/index.tsx
@@ -25,6 +25,41 @@ export function SwiperContinents() {
           subtitle="O continente mais antigo."
         />
       </SwiperSlide>
+      <SwiperSlide>
+        <SwiperItem
+          img="/images/continents/africa.jpg"
+          continent="África"
+          subtitle="Onde há muitos animais."
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperItem
+          img="/images/continents/asia.jpg"
+          continent="Ásia"
+          subtitle="Sua maravilhosa cultura oriental."
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperItem
+          img="/images/continents/north_america.jpg"
+          continent="América do Norte"
+          subtitle="Baixo preço em tecnologias."
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperItem
+          img="/images/continents/south_america.jpg"
+          continent="América do Sul"
+          subtitle="O continente dos maiores rios do planeta."
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SwiperItem
+          img="/images/continents/oceania.jpg"
+          continent="Oceânia"
+          subtitle="Continente com milhares de ilhas paradisíacas"
+        />
+      </SwiperSlide>
     </Swiper>
   )
 }
